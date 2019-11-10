1b33ff2dfb90485119f59fbb56dae75542a53f37
Neil Gowran <neil@wpbeaches.com>
2019-11-10 15:25:24 +1100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,108 +1,39 @@
-// Install packahes as listed in package.json file by running 'npm i' from working directory
-// http://stackoverflow.com/questions/34922749/how-to-install-multiple-gulp-packages-at-once-using-node
-
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var browserSync = require('browser-sync').create();
-var pixrem = require('gulp-pixrem');
-var autoprefixer = require('gulp-autoprefixer');
-var imagemin = require('gulp-imagemin');
-var pngquant = require('imagemin-pngquant');
-
-var sitename = 'mysite'; // set your siteName here
-var username = 'neilg'; // set your macOS userName here
+// Install packages as listed in package.json file by running 'npm i' from working directory
+// Updated for Gulp 4 & Node 12
 
 
-var pxtorem = require('gulp-pxtorem');
-
-var pxtoremOptions = {
-    replace: false,
-	rootValue: 10
-};
-
-var postcssOptions = {
-    map: true
-};
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+const sitename = 'shrink'; // set your siteName here
+const username = 'neilg'; // set your macOS userName here
 
 
-
-// Example
-gulp.task('logo', function() {
-	gutil.log('Workflows suck');
-});
-
-// Act on a specific file - this was going the other way - work in rems and it adds px
-gulp.task('remy', function(){
-	gulp.src('style.css')
-		.pipe(pixrem({ rootValue: 10 }))
-		.pipe(gulp.dest('newstyle'));
-});
-
-
-
-// https://github.com/cuth/gulp-pxtorem
-// https://github.com/cuth/postcss-pxtorem
-gulp.task('rem', function() {
-    gulp.src('style.css')
-		.pipe(pxtorem(pxtoremOptions, postcssOptions))
-        .pipe(gulp.dest('.'));
-});
-
-// Autoprefix last 3 versions - override existing stylesheet
-gulp.task('prefix', function () {
-	return gulp.src('style.css')
-		.pipe(autoprefixer({
-			browsers: ['last 3 versions'],
-			cascade: false
-		}))
-		.pipe(gulp.dest('./'));
-});
-
-
-// Fire up browserSync
-gulp.task('sync', function() {
+  function watch() {
     browserSync.init({
-        proxy: sitename +'.test',
-	xip: true,
-	// or https
-// 	proxy: 'https://' + sitename + '.test',
-//         host: sitename + '.test',
-//         open: 'external',
-//         port: 8000,
-//         https: {
-//             key:
-//                 '/Users/' + username + '/.config/valet/Certificates/' + sitename + '.test.key',
-//             cert:
-//                 '/Users/' + username + '/.config/valet/Certificates/' + sitename + '.test.crt',
-//   	 },	
-	    
-	files: ["./*.css","*.php","css/*css","lib/*.php", "includes/*.php", "includes-child/*.php", "includes-child/woocommerce/*.php", "includes-child/woocommerce/*.css"],
-	
 
+       proxy: sitename +'.test',
+        // or if site is https comment out above line and uncomment lines below
+      	// proxy: 'https://' + sitename + '.test',
+        //       host: sitename + '.test',
+        //       open: 'external',
+        //       port: 8000,
+        //       https: {
+        //           key:
+        //               '/Users/' + username + '/.config/valet/Certificates/' + sitename + '.test.key',
+        //           cert:
+        //               '/Users/' + username + '/.config/valet/Certificates/' + sitename + '.test.crt',
+        // 	      },	
     });
 
-});
+	// Watched files paths
+	gulp.watch('./*.php').on('change',browserSync.reload);
+	gulp.watch('./includes-child/*.php').on('change',browserSync.reload);
+	gulp.watch('./includes/*.php').on('change',browserSync.reload);
+	gulp.watch('./js/*.js').on('change', browserSync.reload);
+	gulp.watch('./css/*.css').on('change', browserSync.reload);
+	gulp.watch('./*css').on('change', browserSync.reload);
+	gulp.watch('./includes-child/woocommerce/*.php').on('change',browserSync.reload);
+	gulp.watch('./includes-child/woocommerce/*.css').on('change',browserSync.reload);
+}
 
-// Watching a file
-gulp.task('watch', function(){
-	gulp.watch('style.css', ['remy']);
-});
-
-
-// Compressing the images
-gulp.task('compress', () => {
-    return gulp.src('./../../uploads/**/*')
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [
-                {removeViewBox: false},
-                {cleanupIDs: false}
-            ],
-            use: [pngquant()]
-        }))
-        .pipe(gulp.dest('./../../uploads/'));
-
-});
-
-// The default task - just run gulp - pass in more tasks by adding the task in the array
-gulp.task('default',['sync', 'compress']);
+exports.watch = watch;
